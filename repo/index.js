74257404b67067549c4f7b246a7f2239574f6913
gio1010gamer <145196820+gio1010gamer@users.noreply.github.com>
2026-07-16 23:05:30 +0200

    username feature update

--- repo/index.js
+++ repo/index.js
@@ -1,8 +1,20 @@
 // Username settings for repo index.html. Once submit, fetches the input
 
 let username;
+const p1 = document.getElementById("p1");
 
 document.getElementById("usernameSubmit").onclick = function() {
     username = document.getElementById("usernameInput").value;
-    document.getElementById("p1").textContent = `Hello ${username}!`;
+    if(username === "me"){
+        p1.textContent = `Hello yourself!`;
+    }else if(username === "you"){
+        p1.textContent = `You ain't me blud`;
+    }else if(username === "gigachad"){
+        p1.textContent = `no you're not?`;
+    }else if(username === "nan" || username === "nan-win93"){
+        p1.textContent = `nope`;
+    }else{
+        p1.textContent = `hello ${username}!`;
+    }
+    
 };
