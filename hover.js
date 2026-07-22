576bdadba62430ca59a4f07c9352292694f5a90d
gio1010gamer <145196820+gio1010gamer@users.noreply.github.com>
2026-07-22 18:55:09 +0200

    commit one

---
+++ hover.js
@@ -0,0 +1,18 @@
+//github link hover
+const github = document.getElementById("githubHover");
+
+github.mouseenter = function(){
+    github.src = link-images/hover/cydiarepo_hover.png
+}
+
+
+
+//cydia repo link hover
+const cydia = document.getElementById("cydiaHover");
+
+cydia.onmouseenter = function(){
+    cydia.src = "link-images/hover/cydiarepo_hover.png";
+}
+cydia.onmouseleave = function(){
+    cydia.src = "link-images/normal/cydiarepo.png"
+}
